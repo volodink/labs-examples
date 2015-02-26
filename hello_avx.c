e303abdee0b1131c689e2c742160687032199890
Konstantin Volodin <volodin.konstantin@gmail.com>
2015-02-26 21:00:34 +0300

    Update hello_avx.c

--- hello_avx.c
+++ hello_avx.c
@@ -3,7 +3,7 @@
 int main()
 {
 	
-	if (__builtin_cpu_supports("avx") && __builtin_cpu_supports("avx512")) {
+	if (__builtin_cpu_supports("avx") && __builtin_cpu_supports("avx2")) {
 	    printf("Hello AVX!");
 	}
 	else {
